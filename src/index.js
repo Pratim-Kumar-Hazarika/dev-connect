013654ce0251938dbe29357200009bce2ee8d18e
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-15 00:10:36 +0530

    add : chakra + router to app

--- src/index.js
+++ src/index.js
@@ -5,17 +5,20 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router} from "react-router-dom"
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <Router>
+        <ChakraProvider>
       <App />
+      </ChakraProvider>
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
