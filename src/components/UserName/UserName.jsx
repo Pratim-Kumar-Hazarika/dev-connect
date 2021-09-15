9afbe51d97b87f364f3c2fd46a52e3abe8e2be2a
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-15 20:49:53 +0530

    Create UserName.jsx

---
+++ src/components/UserName/UserName.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Text, Center, Heading} from "@chakra-ui/react"
+export default function UserName() {
+    return (
+        <Center mt={8} flexDirection="column">
+            <Heading fontSize="xl">@prratim__</Heading>
+            <Text mt={3}>he/him</Text>
+        </Center>
+    )
+}
