c142982a6061ca998cb97fde82c4ca5fb0435334
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-15 20:49:22 +0530

    Create UserFollowersFollowingPosts.jsx

---
+++ src/components/Following/UserFollowersFollowingPosts.jsx
@@ -0,0 +1,17 @@
+import { useDisclosure } from '@chakra-ui/hooks'
+import { Center ,Box} from '@chakra-ui/layout'
+import React from 'react'
+import Model from '../Model/Model'
+
+export default function UserFollowersFollowingPosts({count,text,btnText}) {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    return (
+        <Center flexDirection="column" onClick={onOpen} cursor="pointer">
+            <Box>
+               {count}
+            </Box>
+             <Box >{text}</Box>
+            <Model text={text} btnText={btnText} count={count} isOpen={isOpen} onClose={onClose}/>
+        </Center >
+    )
+}
