9829c2fabe36dc3a538d039558b7ef7ba3a4d4c1
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-15 20:49:45 +0530

    Create Svgs.jsx

---
+++ src/components/Svgs.jsx
@@ -0,0 +1,18 @@
+
+export function MdiGrid(props) {
+    return (
+      <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><path d="M10 4v4h4V4h-4m6 0v4h4V4h-4m0 6v4h4v-4h-4m0 6v4h4v-4h-4m-2 4v-4h-4v4h4m-6 0v-4H4v4h4m0-6v-4H4v4h4m0-6V4H4v4h4m2 6h4v-4h-4v4M4 2h16a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2H4c-1.08 0-2-.9-2-2V4a2 2 0 0 1 2-2z" fill="currentColor"></path></svg>
+    )
+  }
+  
+  export function UilBookmarkFull(props) {
+    return (
+      <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><path d="M18 2H6a1 1 0 0 0-1 1v18a1 1 0 0 0 1.65.76L12 17.27l5.29 4.44A1 1 0 0 0 18 22a.84.84 0 0 0 .38-.08A1 1 0 0 0 19 21V3a1 1 0 0 0-1-1zm-1 16.86l-4.36-3.66a1 1 0 0 0-1.28 0L7 18.86V4h10z" fill="currentColor"></path></svg>
+    )
+  }
+  
+  export function RiVideoLine(props) {
+    return (
+      <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><path d="M3 3.993C3 3.445 3.445 3 3.993 3h16.014c.548 0 .993.445.993.993v16.014a.994.994 0 0 1-.993.993H3.993A.994.994 0 0 1 3 20.007V3.993zM5 5v14h14V5H5zm5.622 3.415l4.879 3.252a.4.4 0 0 1 0 .666l-4.88 3.252a.4.4 0 0 1-.621-.332V8.747a.4.4 0 0 1 .622-.332z" fill="currentColor"></path></svg>
+    )
+  }
