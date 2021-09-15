05273138e5145a18be9bbf059f996cf9e4fec70f
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-15 20:49:33 +0530

    Create Model.jsx

---
+++ src/components/Model/Model.jsx
@@ -0,0 +1,43 @@
+import React, {useState} from 'react'
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+    Center,
+    Box,
+    VStack,
+    Flex,
+    Spacer,
+    Avatar,
+    Text
+} from "@chakra-ui/react"
+import FollowerFollowingModel from '../Followers/FollowerFollowingModel'
+export default function Model({text, btnText, count, isOpen, onClose}) {
+    const [scrollBehavior,setScrollBehavior] = useState("inside")
+    return (
+        <Modal
+            isOpen={isOpen}
+            onClose={onClose}
+            isCentered
+            border="10px solid black"
+            scrollBehavior={scrollBehavior}>
+            <ModalOverlay/>
+            <ModalContent maxH={"50vh"}>
+                <Center>
+                    <ModalHeader>{text}</ModalHeader>
+                </Center>
+                <hr/>
+                <ModalCloseButton/>
+                <ModalBody >
+                    <FollowerFollowingModel btnText={btnText}/>
+                </ModalBody>
+                <ModalFooter></ModalFooter>
+            </ModalContent>
+        </Modal>
+    )
+}
