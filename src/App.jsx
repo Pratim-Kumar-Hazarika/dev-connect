bfbc816bc047e93e215f47667c5d3d1e63251a8a
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-15 20:49:17 +0530

    beautify

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import './App.css';
 import LoginScreen from './Screens/LoginScreen';
+import ProfileScreen from './Screens/ProfileScreen';
 import SignupScreen from './Screens/SignupScreen';
 
-
 function App() {
-  return (
-    <div className="App">
-     <Routes>
-        <Route path="/login" element={<LoginScreen />} />
-        <Route path="/signup" element={<SignupScreen />} />
-    </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/login" element={< LoginScreen />}/>
+                <Route path="/signup" element={< SignupScreen />}/>
+                <Route path="/profile" element={< ProfileScreen />}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
