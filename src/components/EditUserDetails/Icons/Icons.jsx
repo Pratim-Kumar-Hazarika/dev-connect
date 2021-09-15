73bfd6a48221a3bf68deb463b6dbac5664eb7d49
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-16 00:29:20 +0530

    Create Icons.jsx

---
+++ src/components/EditUserDetails/Icons/Icons.jsx
@@ -0,0 +1,11 @@
+export function MdiGenderMale(props) {
+    return (
+      <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><path d="M9 9c1.29 0 2.5.41 3.47 1.11L17.58 5H13V3h8v8h-2V6.41l-5.11 5.09c.7 1 1.11 2.2 1.11 3.5a6 6 0 0 1-6 6a6 6 0 0 1-6-6a6 6 0 0 1 6-6m0 2a4 4 0 0 0-4 4a4 4 0 0 0 4 4a4 4 0 0 0 4-4a4 4 0 0 0-4-4z" fill="currentColor"></path></svg>
+    )
+  }
+
+export function MdiGenderFemale(props) {
+    return (
+      <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><path d="M12 4a6 6 0 0 1 6 6c0 2.97-2.16 5.44-5 5.92V18h2v2h-2v2h-2v-2H9v-2h2v-2.08c-2.84-.48-5-2.95-5-5.92a6 6 0 0 1 6-6m0 2a4 4 0 0 0-4 4a4 4 0 0 0 4 4a4 4 0 0 0 4-4a4 4 0 0 0-4-4z" fill="currentColor"></path></svg>
+    )
+  }
