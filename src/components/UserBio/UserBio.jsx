d246b1b554aa4ac9ae1e14311638ff3ac29da6e6
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-15 20:49:50 +0530

    Create UserBio.jsx

---
+++ src/components/UserBio/UserBio.jsx
@@ -0,0 +1,11 @@
+import {Center, Text} from '@chakra-ui/layout'
+import React from 'react'
+
+export default function UserBio() {
+    return (
+        <Center mt={9}>
+            <Text fontSize="lg">Developer,burger ,dard friends + long flights🌴
+            </Text>
+        </Center>
+    )
+}
