e408e93b3673322bf0a2d1b60bc7316b48b64572
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-16 00:29:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {Routes, Route} from "react-router-dom";
 import './App.css';
+import HomeScreen from './Screens/HomeScreen';
 import LoginScreen from './Screens/LoginScreen';
 import ProfileScreen from './Screens/ProfileScreen';
+import SettingsScreen from './Screens/SettingsScreen';
 import SignupScreen from './Screens/SignupScreen';
 
 function App() {
@@ -11,7 +13,9 @@ function App() {
             <Routes>
                 <Route path="/login" element={< LoginScreen />}/>
                 <Route path="/signup" element={< SignupScreen />}/>
-                <Route path="/profile" element={< ProfileScreen />}/>
+                <Route path ="/" element ={<HomeScreen/>}/>
+                <Route path="/account" element={< ProfileScreen />}/>
+                <Route path="/account/settings" element={< SettingsScreen />}/>
             </Routes>
         </div>
     );
