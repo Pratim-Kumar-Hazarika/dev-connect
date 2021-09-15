ab5d982254f29550e6df251d43782a57d5ed059e
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-16 00:29:25 +0530

    Create PostCaption.jsx

---
+++ src/components/FeedPosts/Components/PostCaption.jsx
@@ -0,0 +1,10 @@
+import {Box, Text} from '@chakra-ui/react'
+import React from 'react'
+
+export default function PostCaption() {
+    return (
+        <Box mt={2} ml={4}>
+            <Text fontSize="sm" fontWeight="medium">Feeling like fucked up !!</Text>
+        </Box>
+    )
+}
