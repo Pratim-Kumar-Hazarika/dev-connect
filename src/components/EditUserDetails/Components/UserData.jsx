3d5525b763da5bb9863e5e2599a7652182317787
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-16 00:29:15 +0530

    Create UserData.jsx

---
+++ src/components/EditUserDetails/Components/UserData.jsx
@@ -0,0 +1,20 @@
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { Input } from "@chakra-ui/input";
+import { Box, Flex } from "@chakra-ui/layout";
+
+ export default function UserData({labelName,placeholder}){
+    return<>
+     <Box width="500px" >
+              <FormControl id="email"  >
+              <Flex  justifyContent="space-around">
+              <Box  width="20%">
+                  <FormLabel>{labelName}</FormLabel>
+                  </Box>
+                  <Box width="70%">
+                      <Input type="name" placeholder={placeholder}/>
+                  </Box>
+              </Flex>
+          </FormControl>
+              </Box>
+    </>
+}
