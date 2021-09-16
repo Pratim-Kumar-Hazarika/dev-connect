d39a8ddedf8190d9f0027f7cbb46e3c306959acc
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-16 21:35:25 +0530

    add : tweet component

--- src/Screens/HomeScreen.jsx
+++ src/Screens/HomeScreen.jsx
@@ -2,11 +2,13 @@ import { Box } from '@chakra-ui/layout'
 import React from 'react'
 import FeedPosts from '../components/FeedPosts/FeedPosts'
 import Header from '../components/Header/Header'
+import Tweet from '../components/Tweet/Tweet'
 export default function HomeScreen() {
     return (
         <Box>
             <Header/>
             {/* Tweet component */}
+            <Tweet/>
             {/* ---- */}
             {/* Flex [posts , Follow users] */}
             {/* First do posts */}
