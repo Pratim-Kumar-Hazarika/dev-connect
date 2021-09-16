cafbdca034cf12bfd5cc55848c3048da877ef35c
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-16 21:34:09 +0530

    navigate :link: to account

--- src/components/Followers/FollowerFollowingModel.jsx
+++ src/components/Followers/FollowerFollowingModel.jsx
@@ -8,7 +8,7 @@ import {
     Button
 } from '@chakra-ui/react'
 import React from 'react'
-
+import {Link} from "react-router-dom"
 export default function FollowerFollowingModel({btnText}) {
     return (
         <VStack spacing={4} align="stretch">
@@ -19,7 +19,9 @@ export default function FollowerFollowingModel({btnText}) {
                             size="md"
                             name="Ryan Florence"
                             src="https://static.highsnobiety.com/thumbor/vQLL2siTyzzbG_eq0wWUMFudvDs=/1600x1067/static.highsnobiety.com/wp-content/uploads/2018/07/25125520/ronaldo-medical-stats-01.jpg"/>
+                        <Link to={`/account/${562728}`}>
                         <Text ml={4} fontWeight="bold">Cristiano Ronaldo</Text>
+                        </Link>
                     </Flex>
                 </Box>
                 <Spacer/>
