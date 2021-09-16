0b3f50fb9c64ebdc903171eb8fa99e3aa7488dca
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-16 21:34:46 +0530

    Create Icons.jsx

---
+++ src/components/Tweet/Icons/Icons.jsx
@@ -0,0 +1,19 @@
+export function MdiFileImageBox(props) {
+    return (
+        <svg width="1.3em" height="1.3em" viewBox="0 0 24 24" {...props}>
+            <path
+                d="M8.5 13.498l2.5 3.006l3.5-4.506l4.5 6H5m16 1v-14a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2z"
+                fill="currentColor"></path>
+        </svg>
+    )
+}
+
+export function MakiCross(props) {
+    return (
+        <svg width="1em" height="1em" viewBox="0 0 15 15" {...props}>
+            <path
+                d="M2.64 1.27L7.5 6.13l4.84-4.84A.92.92 0 0 1 13 1a1 1 0 0 1 1 1a.9.9 0 0 1-.27.66L8.84 7.5l4.89 4.89A.9.9 0 0 1 14 13a1 1 0 0 1-1 1a.92.92 0 0 1-.69-.27L7.5 8.87l-4.85 4.85A.92.92 0 0 1 2 14a1 1 0 0 1-1-1a.9.9 0 0 1 .27-.66L6.16 7.5L1.27 2.61A.9.9 0 0 1 1 2a1 1 0 0 1 1-1c.24.003.47.1.64.27z"
+                fill="currentColor"></path>
+        </svg>
+    )
+}
