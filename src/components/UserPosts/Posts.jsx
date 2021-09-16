62c8fea835214eddb0d8fda376910e1f08cf5fc1
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-16 21:35:07 +0530

    add :link:

--- src/components/UserPosts/Posts.jsx
+++ src/components/UserPosts/Posts.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import {Box, Grid, Image} from "@chakra-ui/react"
 import "./Posts.css"
+import {Link} from "react-router-dom"
 export default function Posts() {
     return (
         <Box className="posts">
             <Grid templateColumns="repeat(3, 1fr)" gap={1}>
                 <Box maxWidth="100%">
+                    <Link to={`/post/${1783920347491}`}>
                     <Image
                         boxSize="100%"
                         objectFit="cover"
                         src="https://images.genius.com/621bdd4975845663c246fc8b8fdcee9c.1000x1000x1.jpg"/>
+                 </Link>
                 </Box>
             </Grid>
         </Box>
