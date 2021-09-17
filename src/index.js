97d85dbc3fda263012cc9143e117332048ac5032
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-18 00:18:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { store } from './app/store';
+import store  from './app/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router} from "react-router-dom"
 import { ChakraProvider } from "@chakra-ui/react";
-
+console.log(store.getState())
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -21,4 +20,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorker.unregister();
+
