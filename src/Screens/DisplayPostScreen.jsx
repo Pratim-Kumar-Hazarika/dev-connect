504fd4988f9f5402215798a1793769696959abe6
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-18 00:18:40 +0530

    Update DisplayPostScreen.jsx

--- src/Screens/DisplayPostScreen.jsx
+++ src/Screens/DisplayPostScreen.jsx
@@ -1,5 +1,7 @@
 import { Box } from '@chakra-ui/layout'
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router'
 import PostCaption from '../components/FeedPosts/Components/PostCaption'
 import PostLikeComment from '../components/FeedPosts/Components/PostLikeComment'
 import UserNameTime from '../components/FeedPosts/Components/UserNameTime'
@@ -7,6 +9,13 @@ import Header from '../components/Header/Header'
 import DisplayPostImage from '../components/Image/DisplayPostImage'
 
 export default function DisplayPostScreen() {
+    const {postId} = useParams()
+    console.log({postId})
+    const posts = useSelector(state => state.post)
+    console.log({posts})
+    const getPostDetails = posts.post.filter((post)=>post._id === String(postId))
+    console.log({getPostDetails})
+    const {_id,image,caption} = getPostDetails[0]
     return (
         <Box>
             <Header/>
@@ -19,8 +28,8 @@ export default function DisplayPostScreen() {
 
             >
             <UserNameTime/>
-            <PostCaption/>
-            <DisplayPostImage/>
+            <PostCaption caption={caption}/>
+            <DisplayPostImage imageSrc={image}/>
             <PostLikeComment/>
         </Box >
         </Box>
