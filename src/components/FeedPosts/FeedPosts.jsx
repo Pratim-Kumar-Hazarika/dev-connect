0003b314fe0a12f1a79555594c21f5443863f473
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-18 00:17:26 +0530

    Update FeedPosts.jsx

--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -5,27 +5,45 @@ import {
     Avatar,
     Grid,
     Image,
-    Button
+    Button,
+    Spacer
 } from '@chakra-ui/react'
 import React from 'react'
+import {useSelector} from 'react-redux'
 import PostCaption from './Components/PostCaption'
 import PostImage from './Components/PostImage'
 import PostLikeComment from './Components/PostLikeComment'
+import PostPopOver from './Components/PostPopOver'
 import UserNameTime from './Components/UserNameTime'
 import "./FeedPosts.css"
+import {MdiDotsVertical} from './Icons/Icons'
+
 export default function FeedPosts() {
+
+    const posts = useSelector(state => state.post)
     return (
-        <Box
-            mt={5}
-            border="1px solid #dbdbdb"
-            p={3}
-            borderRadius="5px"
-            className="post_card"
-            >
-            <UserNameTime/>
-            <PostCaption/>
-            <PostImage/>
-            <PostLikeComment/>
-        </Box >
+        <Box mt={4}>
+            {posts?.post?.map(({_id, image, caption}) => (
+                    <Box
+                        border="1px solid #dbdbdb"
+                        p={3}
+                        borderRadius="5px"
+                        className="post_card"
+                        marginTop="1rem">
+                        <Flex>
+                            <UserNameTime/>
+                            <Spacer/>
+                            <Box>
+                                <PostPopOver _id={_id}/>
+                            </Box>
+                        </Flex>
+
+                        <PostCaption caption={caption} _id={_id}/>
+                        <PostImage imageSrc={image} _id={_id}/>
+                        <PostLikeComment/>
+                    </Box>
+                ))
+}
+        </Box>
     )
 }
