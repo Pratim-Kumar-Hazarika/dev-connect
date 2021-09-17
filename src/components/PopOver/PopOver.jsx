35b61781b22d66fffe539cf2ac78b6ebdcc4d9f6
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-18 00:18:02 +0530

    Update PopOver.jsx

--- src/components/PopOver/PopOver.jsx
+++ src/components/PopOver/PopOver.jsx
@@ -15,9 +15,16 @@ import {
 } from "@chakra-ui/react";
 import React from "react";
 import {IconParkOutlineSetting, RadixIconsAvatar, TeenyiconsHomeOutline} from "./PopOverIconsSvg";
-import {Link} from "react-router-dom"
-export function PopOver() {
-
+import {Link,useNavigate} from "react-router-dom"
+import {useDispatch} from "react-redux";
+import {logoutButtonPressed} from "../../features/auth/authSlice";
+export function PopOver({imgSrc}) {
+    const dispatch = useDispatch() 
+    const navigate = useNavigate()
+    function logoutHandler() {
+        dispatch(logoutButtonPressed())
+        navigate("/login")
+    }
     return (
         <Popover >
             <PopoverTrigger>
@@ -25,45 +32,51 @@ export function PopOver() {
                     cursor="pointer"
                     size="sm"
                     name="Ryan Florence"
-                    src="https://static.highsnobiety.com/thumbor/vQLL2siTyzzbG_eq0wWUMFudvDs=/1600x1067/static.highsnobiety.com/wp-content/uploads/2018/07/25125520/ronaldo-medical-stats-01.jpg"/>
+                    src={imgSrc}/>
             </PopoverTrigger>
             <PopoverContent>
                 <PopoverArrow/>
                 <PopoverCloseButton/>
                 <PopoverBody>
-                <Link to="/account">
-                    <Flex alignItems="center">
-                        <RadixIconsAvatar/>
-                        <Text ml={3}>
-                           
+                    <Link to="/account">
+                        <Flex alignItems="center">
+                            <RadixIconsAvatar/>
+                            <Text ml={3}>
+
                                 Profile
-                        </Text>
-                    </Flex>
+                            </Text>
+                        </Flex>
                     </Link>
                 </PopoverBody>
                 <PopoverBody>
-                <Link to="/">
-                    <Flex>
-                        <TeenyiconsHomeOutline/>
-                        <Text ml={3}>
-                            Home</Text>
-                    </Flex>
+                    <Link to="/">
+                        <Flex>
+                            <TeenyiconsHomeOutline/>
+                            <Text ml={3}>
+                                Home</Text>
+                        </Flex>
                     </Link>
                 </PopoverBody>
                 <PopoverBody>
-                <Link to="/account/settings">
-                    <Flex>
-                        <IconParkOutlineSetting className="popover_icons"/>
-                        <Text ml={3}>
-                           
+                    <Link to="/account/settings">
+                        <Flex>
+                            <IconParkOutlineSetting className="popover_icons"/>
+                            <Text ml={3}>
+
                                 Settings
-                        </Text>
-                    </Flex>
+                            </Text>
+                        </Flex>
                     </Link>
                 </PopoverBody>
                 <PopoverBody>
                     <hr/>
-                    <Button mt={"2px"} variant="ghost" size="md" fontSize="md" fontWeight="medium">
+                    <Button
+                        onClick={logoutHandler}
+                        mt={"2px"}
+                        variant="ghost"
+                        size="md"
+                        fontSize="md"
+                        fontWeight="medium">
                         Log Out
                     </Button>
                 </PopoverBody>
