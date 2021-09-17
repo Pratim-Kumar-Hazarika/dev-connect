8d46ab39c05abfe46b0607b8108f8e9142be2d74
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-18 00:18:20 +0530

    Update UplodImage.jsx

--- src/components/UplodImage.jsx
+++ src/components/UplodImage.jsx
@@ -1,12 +1,15 @@
 
 import { Button } from "@chakra-ui/button";
-import { FormLabel } from "@chakra-ui/form-control";
+import { FormLabel ,Text} from "@chakra-ui/react";
 import { Input } from "@chakra-ui/input";
 import { Box } from "@chakra-ui/layout";
 import { Tooltip } from "@chakra-ui/tooltip";
 import { MdiFileImageBox } from "./Tweet/Icons/Icons";
+import {useLocation} from "react-router-dom"
 
 export default function UplodImage({handeInputChange}) {
+
+    const query = useLocation()
     return <Box>
         <Button
             size="xs"
@@ -16,9 +19,10 @@ export default function UplodImage({handeInputChange}) {
             fontWeight="medium"
             border="1px solid #dbdbdb">
             <FormLabel mt={2} mr={1} cursor="pointer">
-                <Tooltip label="Add Image" aria-label="A tooltip">
+                <Tooltip label={ query.pathname === "/account/settings" ? '': 'Add Image'} aria-label="A tooltip">
                     <Box color="gray">
-                        <MdiFileImageBox />
+                        { query.pathname === "/account/settings" ?  <Text fontSize="md" fontWeight="medium" color="#0095f6">Change Profile Picture</Text> :   <MdiFileImageBox />}
+                     
                     </Box>
                 </Tooltip>
                 <Input type="file" display="none" onChange={handeInputChange} />
