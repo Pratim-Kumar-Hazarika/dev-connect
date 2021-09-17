572b0f28c93dfdb57c8eb772a2ba2039f2082d93
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-18 00:16:50 +0530

    add : props

--- src/components/FeedPosts/Components/PostCaption.jsx
+++ src/components/FeedPosts/Components/PostCaption.jsx
@@ -1,10 +1,12 @@
 import {Box, Text} from '@chakra-ui/react'
 import React from 'react'
-
-export default function PostCaption() {
+import {Link} from "react-router-dom"
+export default function PostCaption({caption,_id}) {
     return (
         <Box mt={2} ml={4}>
-            <Text fontSize="sm" fontWeight="medium">Feeling like fucked up !!</Text>
+              <Link to={`post/${ _id}`}>
+            <Text fontSize="sm" fontWeight="medium">{caption}</Text>
+            </Link>
         </Box>
     )
 }
