5dd7c0540a8366fbbd40eb4c3b560fed6d5fa6dd
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-18 00:16:16 +0530

    add : slices

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from "../features/auth/authSlice"
+import postSlice from '../features/post/postSlice';
+import profileSettingsSlice from '../features/settings/profileSettingsSlice';
 
-export const store = configureStore({
+export default configureStore({
   reducer: {
-    authentication : authSlice
+    authentication : authSlice,
+    post :postSlice,
+    profileSettings:profileSettingsSlice
   },
 });
