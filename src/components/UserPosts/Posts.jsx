689207a49d94adb3f2eb484ee773fccc8242cff2
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-18 00:18:22 +0530

    Update Posts.jsx

--- src/components/UserPosts/Posts.jsx
+++ src/components/UserPosts/Posts.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
-import {Box, Grid, Image} from "@chakra-ui/react"
+import {Box, Grid, Image,Text} from "@chakra-ui/react"
 import "./Posts.css"
-import {Link} from "react-router-dom"
+import {Link,useParams} from "react-router-dom"
+import {useSelector} from 'react-redux'
 export default function Posts() {
+    const posts = useSelector(state => state.post.post.filter((post)=>post.flag === "image-exists")) ||{}
     return (
         <Box className="posts">
             <Grid templateColumns="repeat(3, 1fr)" gap={1}>
-                <Box maxWidth="100%">
-                    <Link to={`/post/${1783920347491}`}>
-                    <Image
-                        boxSize="100%"
-                        objectFit="cover"
-                        src="https://images.genius.com/621bdd4975845663c246fc8b8fdcee9c.1000x1000x1.jpg"/>
-                 </Link>
-                </Box>
+                {
+                        posts?.map(({_id, image}) => (
+                            <Box maxWidth="100%">
+                                <Link to={`/post/${ _id}`}>
+                                    <Image boxSize="100%" objectFit="cover"  src={image}/>
+                                </Link>
+                            </Box>
+                        ))
+            }
             </Grid>
         </Box>
 
