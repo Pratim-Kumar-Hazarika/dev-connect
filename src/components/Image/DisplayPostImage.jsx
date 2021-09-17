889abb928a4b168917552ca9f694f3072898e1bd
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-18 00:18:00 +0530

    Update DisplayPostImage.jsx

--- src/components/Image/DisplayPostImage.jsx
+++ src/components/Image/DisplayPostImage.jsx
@@ -1,14 +1,14 @@
 import {Box, Grid, Image} from '@chakra-ui/react'
 import React from 'react'
 
-export default function DisplayPostImage() {
+export default function DisplayPostImage({imageSrc}) {
     return (
         <Grid templateColumns="repeat(1, 1fr)" gap={1}>
             <Box maxWidth="100%" ml={4} mt={5}>
                 <Image
                     boxSize="100%"
                     objectFit="cover"
-                    src="https://images.genius.com/621bdd4975845663c246fc8b8fdcee9c.1000x1000x1.jpg"/>
+                    src={imageSrc}/>
             </Box>
         </Grid>
     )
