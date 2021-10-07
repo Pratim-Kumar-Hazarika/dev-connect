d10ff6a3ac9a2d5d4eefc43a2b584a74b452ac9a
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-10-08 01:25:53 +0530

    update : show a user following + followers length

--- src/components/Following/UserFollowersFollowingPosts.jsx
+++ src/components/Following/UserFollowersFollowingPosts.jsx
@@ -4,16 +4,37 @@ import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import Model from '../Model/Model'
 
-export default function UserFollowersFollowingPosts({text,btnText}) {
+export default function UserFollowersFollowingPosts({text,btnText,_id}) {
     const { isOpen, onOpen, onClose } = useDisclosure()
-    const {userFollowers} = useSelector(state => state.userFollowers)
-    const {userfollowings} = useSelector(state => state.userFollowings)
+
+    const {userFollowers,allFollowers} = useSelector(state => state.userFollowers)
+   
+    ///improve naming convention
+    const getFollowers = allFollowers.filter((item)=>item._id === String(_id))
+   
+    const {userfollowings,allFollowings} = useSelector(state => state.userFollowings)
+    const getFollowings = allFollowings.filter((item)=>item._id === String(_id))
+    ////////////
+    console.log({allFollowers})
+    console.log({getFollowings})
+    console.log({allFollowings})
     return (
         <Center flexDirection="column" onClick={onOpen} cursor="pointer">
             <Box>
                 {
-                    text === "Followers" ? userFollowers.length :userfollowings.length
+                    _id ==="admin" ? 
+                    (
+                     text === "Followers" ?
+                      userFollowers.length :userfollowings.length
+                    )
+                    :
+                    ( text === "Followers" ? 
+                        getFollowers[0]?.userDetails.length || 0 
+                                            :
+                        getFollowings[0]?.userDetails.length || 0
+                    )
                 }
+              
             </Box>
              <Box >{text}</Box>
             <Model text={text} btnText={btnText}  isOpen={isOpen} onClose={onClose}/>
