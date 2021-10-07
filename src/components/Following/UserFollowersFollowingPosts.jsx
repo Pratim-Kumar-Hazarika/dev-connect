98c249c7c6e1dc4dc4ba7cadad328d77fdfee917
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-10-08 00:28:54 +0530

    Update UserFollowersFollowingPosts.jsx

--- src/components/Following/UserFollowersFollowingPosts.jsx
+++ src/components/Following/UserFollowersFollowingPosts.jsx
@@ -1,18 +1,22 @@
 import { useDisclosure } from '@chakra-ui/hooks'
 import { Center ,Box} from '@chakra-ui/layout'
 import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import Model from '../Model/Model'
 
-export default function UserFollowersFollowingPosts({count,text,btnText}) {
+export default function UserFollowersFollowingPosts({text,btnText}) {
     const { isOpen, onOpen, onClose } = useDisclosure()
-    
+    const {userFollowers} = useSelector(state => state.userFollowers)
+    const {userfollowings} = useSelector(state => state.userFollowings)
     return (
         <Center flexDirection="column" onClick={onOpen} cursor="pointer">
             <Box>
-               {count}
+                {
+                    text === "Followers" ? userFollowers.length :userfollowings.length
+                }
             </Box>
              <Box >{text}</Box>
-            <Model text={text} btnText={btnText} count={count} isOpen={isOpen} onClose={onClose}/>
+            <Model text={text} btnText={btnText}  isOpen={isOpen} onClose={onClose}/>
         </Center >
     )
 }
