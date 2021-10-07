4089b753512fecdd5c5693683ab8d9625096506f
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-10-08 00:29:01 +0530

    Update ProfilePicture.jsx

--- src/components/ProfilePicture/ProfilePicture.jsx
+++ src/components/ProfilePicture/ProfilePicture.jsx
@@ -5,14 +5,7 @@ import React from 'react'
 export default function ProfilePicture({imageSrc}) {
     return (
         <Center mt={5}>
-           {/* <Image
-                borderRadius="full"
-                boxSize="150px"
-              
-                src={imageSrc}
-                alt="Segun Adebayo"
-                /> */}
-                <Avatar size="2xl" name="Segun Adebayo" src={imageSrc}/>
+                <Avatar size="2xl" name="" src={imageSrc}/>
            </Center>
     )
 }
