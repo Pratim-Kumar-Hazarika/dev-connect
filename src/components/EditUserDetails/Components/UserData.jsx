5b2f557ce7216e825a10c6a3b765fa94659d0fd9
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-10-08 00:28:47 +0530

    Update UserData.jsx

--- src/components/EditUserDetails/Components/UserData.jsx
+++ src/components/EditUserDetails/Components/UserData.jsx
@@ -2,7 +2,7 @@ import { FormControl, FormLabel } from "@chakra-ui/react";
 import { Input } from "@chakra-ui/react";
 import { Box, Flex } from "@chakra-ui/layout";
 
- export default function UserData({labelName,placeholder}){
+ export default function UserData({labelName,placeholder,value,inputHandler}){
     return<>
      <Box width="500px" >
               <FormControl id="email"  >
@@ -11,7 +11,7 @@ import { Box, Flex } from "@chakra-ui/layout";
                   <FormLabel>{labelName}</FormLabel>
                   </Box>
                   <Box width="70%">
-                      <Input type="name" placeholder={placeholder}/>
+                      <Input type="name" placeholder={placeholder} value={value} onChange={inputHandler}/>
                   </Box>
               </Flex>
           </FormControl>
