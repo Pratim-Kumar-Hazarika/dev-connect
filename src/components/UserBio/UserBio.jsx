54038955a7d03bf780cec011e091268f4bb6eafe
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-10-08 00:29:04 +0530

    Update UserBio.jsx

--- src/components/UserBio/UserBio.jsx
+++ src/components/UserBio/UserBio.jsx
@@ -1,10 +1,10 @@
 import {Center, Text} from '@chakra-ui/layout'
 import React from 'react'
 
-export default function UserBio() {
+export default function UserBio({userbio}) {
     return (
         <Center mt={9}>
-            <Text fontSize="lg">Developer,burger ,dard friends + long flights🌴
+            <Text fontSize="lg">{userbio}
             </Text>
         </Center>
     )
