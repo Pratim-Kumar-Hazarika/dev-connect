3ac1484622a02a6bfdd8c856af700af766ab25ae
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-10-08 00:23:43 +0530

    Update profileSettingsSlice.js

--- src/features/settings/profileSettingsSlice.js
+++ src/features/settings/profileSettingsSlice.js
@@ -1,9 +1,26 @@
-import { createSlice ,current} from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice ,current} from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getUserDetailsFromServer = createAsyncThunk("userProfileDetails/profileSettingSlice",async({token})=>{
+    const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/user/details`,{
+        headers: {
+            authorization: token
+        }})
+    return response.data.profileDetails;
+})
 
 const profileSettingsSlice = createSlice({
     name:"profileSettings",
     initialState:{
-        profilePicture: null
+        profilePicture: null,
+        name :null,
+        username :null,
+        website:null,
+        bio:null,
+        email:null,
+        gender:null,
+        message:"no request made from profile Slice",
+        userId:null
     },
     reducers:{
         changeProfilePictureButtonClicked(state,action){
@@ -12,7 +29,25 @@ const profileSettingsSlice = createSlice({
             console.log("change after button",current(state),action)
         },
     },
-    extraReducers:{}
+    extraReducers:{
+        [getUserDetailsFromServer.pending]:(state)=>{
+            state.message ="Request made to server to get profile details"
+        },
+        [getUserDetailsFromServer.fulfilled]:(state,action)=>{
+            const{gender,name,profileBio,profilePicture,username,website,email,userId} = action.payload
+            state.message ="Profile details fetched from server"
+            state.profilePicture = profilePicture
+            state.name  =  name
+            state.username  = username
+            state.website = website
+            state.bio = profileBio
+            state.email = email
+            state.gender = gender
+            state.userId=userId
+        },[getUserDetailsFromServer.rejected]:(state)=>{
+            state.message ="Request made to server to get profile details"
+        }
+    }
 })
 export const {changeProfilePictureButtonClicked} = profileSettingsSlice.actions
 export default profileSettingsSlice.reducer;
