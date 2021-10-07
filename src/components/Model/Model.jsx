e3fa5b1901fd225a342bc49498e2845044806ca9
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-10-08 00:28:56 +0530

    Update Model.jsx

--- src/components/Model/Model.jsx
+++ src/components/Model/Model.jsx
@@ -17,8 +17,23 @@ import {
     Text
 } from "@chakra-ui/react"
 import FollowerFollowingModel from '../Followers/FollowerFollowingModel'
+import { useSelector } from 'react-redux'
 export default function Model({text, btnText, count, isOpen, onClose}) {
     const [scrollBehavior,setScrollBehavior] = useState("inside")
+    const {userFollowers} = useSelector(state => state.userFollowers)
+    const {userfollowings} = useSelector(state => state.userFollowings)
+   
+    function userFollowingModel(userfollowings, btnText) {
+        return userfollowings.map(({ _id, username, profilePicture }) => (
+            <FollowerFollowingModel from={"following"} btnText={btnText} username={username} _id={_id} profilePicture={profilePicture} />
+        ))
+    }
+    
+    function userFollowersModel(userFollowers, btnText) {
+        return userFollowers.map(({ _id, username, profilePicture }) => (
+            <FollowerFollowingModel from={"followers"} btnText={btnText} username={username} _id={_id} profilePicture={profilePicture} />
+        ))
+    }
     return (
         <Modal
             isOpen={isOpen}
@@ -33,10 +48,15 @@ export default function Model({text, btnText, count, isOpen, onClose}) {
                 <hr/>
                 <ModalCloseButton/>
                 <ModalBody >
-                    <FollowerFollowingModel btnText={btnText}/>
+                    {
+                        text==="Followers" ? userFollowersModel(userFollowers, btnText) :userFollowingModel(userfollowings, btnText)
+                    }
+                   
                 </ModalBody>
                 <ModalFooter></ModalFooter>
             </ModalContent>
         </Modal>
     )
 }
+
+
