f51f4ee786dbc9546d32160024d28582cd814747
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-10-08 00:28:39 +0530

    Update FeedPosts.jsx

--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -21,9 +21,49 @@ import {MdiDotsVertical} from './Icons/Icons'
 export default function FeedPosts() {
 
     const posts = useSelector(state => state.post)
+    const {userId} = useSelector(state=>state.profileSettings)
+    console.log({userId})
+    const filterPosts = useSelector(state=>state.post.feedPosts)
+    console.log({filterPosts})
+    ////change naming convention later
+    const getProperPosts = filterPosts.map((post)=>({
+        ...post,
+        _id :{
+            userId : post._id._id,
+            userName :post._id.username,
+            userAvatar :post._id.profilePicture || ''
+        }
+    }))
+    console.log({getProperPosts})
     return (
         <Box mt={4}>
-            {posts?.post?.map(({_id, image, caption}) => (
+            {
+                getProperPosts.map((post)=>(
+                    <Box>
+                        {post?.posts?.map(({_id, image, caption}) => (
+                    <Box
+                        border="1px solid #dbdbdb"
+                        p={3}
+                        borderRadius="5px"
+                        className="post_card"
+                        marginTop="1rem">
+                        <Flex>
+                            <UserNameTime userImage={post._id.userAvatar} userName={post._id.userName}/>
+                            <Spacer/>
+                            <Box>
+                                {userId === post._id.userId ?<PostPopOver _id={_id}/>:""}                             
+                            </Box>
+                        </Flex>
+                        <PostCaption caption={caption} _id={_id}/>
+                        <PostImage imageSrc={image} _id={_id}/>
+                        <PostLikeComment/>
+                    </Box>
+                ))
+}
+                    </Box>
+                ))
+            }
+            {/* {posts?.post?.map(({_id, image, caption}) => (
                     <Box
                         border="1px solid #dbdbdb"
                         p={3}
@@ -43,7 +83,7 @@ export default function FeedPosts() {
                         <PostLikeComment/>
                     </Box>
                 ))
-}
+} */}
         </Box>
     )
 }
