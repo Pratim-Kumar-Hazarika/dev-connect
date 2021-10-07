605eedc5d3c3f3948b8424b5008be74f28b26903
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-10-08 00:23:13 +0530

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from "../features/auth/authSlice"
+import followerSlice from '../features/followers/followerSlice';
+import followingSlice from '../features/followings/followingSlice';
 import postSlice from '../features/post/postSlice';
 import profileSettingsSlice from '../features/settings/profileSettingsSlice';
-
+import allUsersSlice from "../features/users/userSlice"
 export default configureStore({
   reducer: {
     authentication : authSlice,
     post :postSlice,
-    profileSettings:profileSettingsSlice
+    profileSettings:profileSettingsSlice,
+    userFollowers :followerSlice,
+    userFollowings:followingSlice,
+    allUsers :allUsersSlice
   },
 });
