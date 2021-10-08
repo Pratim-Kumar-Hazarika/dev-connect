0ba4e0ca0b2e6713dd229d13f3e174715824fdcd
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-10-08 20:13:10 +0530

    Update UserName.jsx

--- src/components/UserName/UserName.jsx
+++ src/components/UserName/UserName.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import {Text, Center, Heading} from "@chakra-ui/react"
-export default function UserName() {
+export default function UserName({username, gender}) {
     return (
         <Center mt={8} flexDirection="column">
-            <Heading fontSize="xl">@prratim__</Heading>
-            <Text mt={3}>he/him</Text>
+            <Heading fontSize="xl">{username}</Heading>
+            <Text mt={3}>{(gender === "male" && "he/him") || (gender === "Prefer not to say!" && "") || (gender === "female" && "she/her")}</Text>
         </Center>
     )
 }
