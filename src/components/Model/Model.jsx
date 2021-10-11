0016f897341945d7d1ca70e74d9da32fa907ec4c
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-10-12 00:19:29 +0530

    update : show post likes users

--- src/components/Model/Model.jsx
+++ src/components/Model/Model.jsx
@@ -18,15 +18,20 @@ import {
 } from "@chakra-ui/react"
 import FollowerFollowingModel from '../Followers/FollowerFollowingModel'
 import { useSelector } from 'react-redux'
-export default function Model({text, btnText, _id, isOpen, onClose}) {
+export default function Model({text, btnText, _id, isOpen, onClose,likedUsers,from}) {
     const [scrollBehavior,setScrollBehavior] = useState("inside")
     const {userFollowers,allFollowers} = useSelector(state => state.userFollowers)
     const {userfollowings,allFollowings} = useSelector(state => state.userFollowings)
-   
+  
     const getFollowers = allFollowers.filter((item)=>item._id === String(_id))
     const getFollowings = allFollowings.filter((item)=>item._id === String(_id))
 
     function userFollowingModel(userfollowings, btnText) {
+        if(from ==="post"){
+            return likedUsers.map(({ _id, username, profilePicture }) => (
+                <FollowerFollowingModel from={"following"} btnText={btnText} username={username} _id={_id} profilePicture={profilePicture} />
+            ))
+        }
         if(_id !== "admin"){
             return getFollowings[0]?.userDetails.map(({ _id, username, profilePicture }) => (
                 <FollowerFollowingModel from={"following"} btnText={btnText} username={username} _id={_id} profilePicture={profilePicture} />
