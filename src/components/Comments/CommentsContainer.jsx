95cdc598b9b204c076055d08faa515f4eb0f05a1
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-10-14 01:03:32 +0530

    add : show comments functionality

--- src/components/Comments/CommentsContainer.jsx
+++ src/components/Comments/CommentsContainer.jsx
@@ -11,26 +11,60 @@ import UserAvatar from '../Avatar/UserAvatar'
 import CommentsText from './CommentsText'
 import Comments from './Comments'
 import { useState } from 'react'
-export default function CommentsContainer({comments, replyHandler}) {
-    // console.log({comments})
-    const[num,setNum] = useState(1)
-function viewHideReplyHandler({childrenCommentsArray}){
-// console.log("clicked",{childrenCommentsArray})
+import { useParams } from 'react-router'
 
-if(num !== childrenCommentsArray.length){
-    setNum(value=>value+1)
-}else{
-    console.log("hide kar")
-    setNum(1)
-}
-}
-function testFunction({childrenArray}){
-console.log({childrenArray})
-}
+export default function CommentsContainer({comments, replyHandler}) {
+    const {postId} = useParams()
+    let filterComments;
+    const [priorityArray,setPriorityArray] = useState()
+    filterComments = priorityArray || comments?.map((comment)=>(
+        {
+            ...comment,
+            childrenComments:comment.childrenComments.slice(0,1)
+        }
+    ));
+    const [pataNehi,setPataNehi] = useState(filterComments);
+    
+        function viewHideReplyHandler({comment,index,parrentCommentId}){
+
+        const getCommentsWhichIsNotPresent = comments[index].childrenComments.filter(comment => !filterComments[index].childrenComments.includes(comment))
+        
+        ///Show only two comments when clicked -> Left--feature
+
+        const partiCularCommentArray = comment.childrenComments.length 
+        const mainCommentsArray =  comments[index].childrenComments.length
+      
+        if(partiCularCommentArray === mainCommentsArray){
+            filterComments = filterComments.map((comment)=>(
+                comment.parrentCommentId === parrentCommentId ?{
+                    ...comment,
+                    childrenComments:comment.childrenComments.slice(0,1)
+                }: comment
+            ))
+            setPriorityArray(filterComments)
+           return setPataNehi(filterComments)
+        } else{
+            filterComments = filterComments.map((comment)=>(
+                comment.parrentCommentId === parrentCommentId ?{
+                    ...comment,
+                    childrenComments:[...comment.childrenComments,getCommentsWhichIsNotPresent[0]]
+                }:comment
+            ))
+            setPriorityArray(filterComments)
+           return setPataNehi(filterComments)
+        }  
+        }
+        function showRepliesNumber({num}){
+            console.log({num})
+            let x = num
+            x = x-1
+            console.log(x)
+return x
+        }
     return (
         <Box border="" mt={2} height={"355px"} overflowX={"scroll"}>
-            {comments
-                ?.map((comment) => (
+            {pataNehi
+                ?.map((comment,index) => (
                     <Box ml={4}  mt={2}border=" ">
                         <Flex alignItems={" "}>
                             <UserAvatar image={comment._id.profilePicture}/>
@@ -47,18 +81,16 @@ console.log({childrenArray})
                                     Reply
                                 </Box>
                                 <Box
-                                    onClick={()=>viewHideReplyHandler({childrenCommentsArray:comment?.childrenComments})}
+                                    onClick={()=>viewHideReplyHandler({comment:comment,postId:postId,index,parrentCommentId:comment.parrentCommentId})}
                                     cursor={"pointer"}
                                     ml={4}
                                     mt={2}
                                     color="gray"
                                     fontWeight={"bold"}
-                                    fontSize={"14px"}>{comment?.childrenComments?.length === 0 ? '' : `-----View${comment?.childrenComments?.length} replies` }
+                                    fontSize={"14px"}>{comment?.childrenComments?.length === comments[index]?.childrenComments?.length? 'Hide Replies' : (comment?.childrenComments?.length ===0 ? `-----Viewss${(comments[index]?.childrenComments?.length) -1} replies` :`-----Viewxxx ${showRepliesNumber({num:comments[index]?.childrenComments?.length}) } replies`) }
                                 </Box>
-                                    {testFunction({childrenArray:comment.childrenComments})}
-                                {comment
-                                    .childrenComments.slice(0,num)
-                                    .map((childrenComment) => (
+                               
+                                {comment?.childrenComments?.map((childrenComment) => (
                                     <Comments
                                         image={childrenComment._id.profilePicture}
                                         userName={childrenComment._id.username}
