4fd0db0cb45a1ee59307bf2358a40ff325eead6b
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-10-14 01:21:32 +0530

    Create viewHideReplyHandler.js

---
+++ src/Utils/viewHideReplyHandler.js
@@ -0,0 +1,31 @@
+export function viewHideReplyHandler({comment,index,parrentCommentId,comments,filterComments,setPriorityArray,setModifiedComments}){
+
+    const getCommentsWhichIsNotPresent = comments[index].childrenComments.filter(comment => !filterComments[index].childrenComments.includes(comment))
+    
+    const partiCularCommentArray = comment.childrenComments.length 
+    const mainCommentsArray =  comments[index].childrenComments.length
+  
+    if(partiCularCommentArray === mainCommentsArray){
+        filterComments = filterComments.map((comment)=>(
+            comment.parrentCommentId === parrentCommentId ?{
+                ...comment,
+                childrenComments:comment.childrenComments.slice(0,1)
+            }: comment
+        ))
+        setPriorityArray(filterComments)
+       return setModifiedComments(filterComments)
+    } else{
+        filterComments = filterComments.map((comment)=>(
+            comment.parrentCommentId === parrentCommentId ?{
+                ...comment,
+                childrenComments:[...comment.childrenComments,getCommentsWhichIsNotPresent[0]]
+            }:comment
+        ))
+        setPriorityArray(filterComments)
+       return setModifiedComments(filterComments)
+    }  
+    }
+
+    export  function showRepliesNumber({num1,num2}){
+        return num1-num2
+      }
