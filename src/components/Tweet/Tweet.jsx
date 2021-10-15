556f74d06f21975d422fbe40ad0757430ba6a446
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-10-16 00:57:22 +0530

    Update Tweet.jsx

--- src/components/Tweet/Tweet.jsx
+++ src/components/Tweet/Tweet.jsx
@@ -23,13 +23,14 @@ import { createPost, newPost } from '../../features/post/postSlice'
 export default function Tweet() {
 
     const postState = useSelector(state => state.post)
+    const allPOSTS = useSelector(state=>state)
     const {token} = useSelector(state=>state.authentication)
     // console.log("yoyo token from tweet.jsx",{token})
     const dispatch = useDispatch()
     const [imageSrc,setImageSrc] = useState()
 
     function handeInputChange(e) {
-        console.log("called from Tweet change")
+        // console.log("called from Tweet change")
         const file = e.target.files[0];
         previewFile(file);
     
@@ -43,10 +44,10 @@ export default function Tweet() {
         };
 
     }
-
+    const {profilePicture,userId,username} = useSelector(state=>state.profileSettings)
     function createPostHandler(){
-        if(inputCaption!==""  ){
-            dispatch(createPost({_id:Math.random(),caption:inputCaption,image:imageSrc}))
+        if(inputCaption  ){
+            dispatch(createPost({_id:String(Math.random()),caption:inputCaption,image:imageSrc,flag: imageSrc?"image-exists":'',userName:username,profilePic:profilePicture,userId:userId,likes:[],comments:[]}))
             setInputCaption(" ")
             setImageSrc("")
             dispatch(newPost({caption:inputCaption,imageSrc:imageSrc,token}))
@@ -54,11 +55,12 @@ export default function Tweet() {
     }
 
     const [inputCaption,setInputCaption] = useState()
-    const {profilePicture} = useSelector(state=>state.profileSettings)
+   
     return (
         <Box
-            className="tweet_box"
-            mt={7}
+        bgColor={"white"}
+            className=""
+            mt={2}
             border="1px solid #dbdbdb"
             borderRadius="4px"
             p={2}
@@ -72,7 +74,7 @@ export default function Tweet() {
                         ml={3}
                         cursor="pointer"
                         size="md"
-                        name="Ryan Florence"
+                        name=""
                         src={profilePicture}/>
                 </Box>
                 <Box flex="1">
@@ -98,6 +100,7 @@ export default function Tweet() {
                                 fontWeight="medium"
                                 width={"200px"}
                                 ml={4}
+                                disabled={inputCaption?false:true}
                                 border="1px solid #dbdbdb">Post</Button>
                         </Box>
                     </Flex>
