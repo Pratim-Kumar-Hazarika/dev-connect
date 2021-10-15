290c00c12069d614b0b84d4dcb744e19fcaee35a
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-10-16 00:58:25 +0530

    Update HomeScreen.jsx

--- src/Screens/HomeScreen.jsx
+++ src/Screens/HomeScreen.jsx
@@ -1,19 +1,28 @@
-import { Box } from '@chakra-ui/layout'
+import {Box, Flex, Spacer, Text} from '@chakra-ui/layout'
 import React from 'react'
+import {Avatar, AvatarBadge, AvatarGroup} from "@chakra-ui/react"
 import FeedPosts from '../components/FeedPosts/FeedPosts'
 import Header from '../components/Header/Header'
 import Tweet from '../components/Tweet/Tweet'
+import {Grid, GridItem} from "@chakra-ui/react"
+import RightSuggestionsBar from '../components/RightSuggestionsBar/RightSuggestionsBar'
+import "./HomeScreen.css"
 export default function HomeScreen() {
     return (
-        <Box>
+        <Box bgColor={"#EFEFEF"}>
             <Header/>
-            {/* Tweet component */}
-            <Tweet/>
-            {/* ---- */}
-            {/* Flex [posts , Follow users] */}
-            {/* First do posts */}
-            {/* {then users} */}
-            <FeedPosts/>
-        </Box>
+            <Box className='main_div'>
+                <div class="wrapper">
+                    <div class="main">
+                        <Tweet/>
+                        <FeedPosts/>
+                    </div>
+
+                    <div class="sidebar">
+                        <RightSuggestionsBar/>
+                    </div>
+                </div>
+            </Box>
+        </Box >
     )
 }
