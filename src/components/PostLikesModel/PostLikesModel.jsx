d68ef2283994f3488d97f4a8b832acb07b59f8d9
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-10-16 00:58:49 +0530

    Update PostLikesModel.jsx

--- src/components/PostLikesModel/PostLikesModel.jsx
+++ src/components/PostLikesModel/PostLikesModel.jsx
@@ -5,21 +5,25 @@ import React, {useEffect} from 'react'
 import {ClarityHeartSolid} from '../FeedPosts/Icons/Icons'
 import Model from '../Model/Model'
 
-export default function PostLikesModel({ text, btnText,totalLikes}) {
+export default function PostLikesModel({ text, btnText,totalLikes,likedUsers}) {
     const {isOpen, onOpen, onClose} = useDisclosure()
+
 
     return (
         <Center flexDirection="column" onClick={onOpen} cursor="pointer">
-            <Button ml={"3px"}
+            {/* <Button ml={"3px"}
                 variant="ghost"
                 size="xs"
                 color="gray"
                 fontWeight="medium"
                 border="1px solid #F9FAFB">
-                  <Text ml={2}>{totalLikes}</Text>
-            </Button>
+                  <Text ml={2}>{totalLikes} Likes</Text>
+            </Button> */}
+            <Text ml={2} mt={1}>{totalLikes} likes</Text>
             <Model
+            likedUsers={likedUsers}
                 text={text}
+                from={"post"}
                 btnText={btnText}
                 // count={count}
                 isOpen={isOpen}
