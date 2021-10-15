9af553dd504e3c2f70c2a49e0f81842ed025ec49
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-10-16 00:57:18 +0530

    Update followingSlice.js

--- src/features/followings/followingSlice.js
+++ src/features/followings/followingSlice.js
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice,current } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const getUserFollowingsFromServer = createAsyncThunk("userfollowings/fromServer",async({token})=>{
@@ -23,7 +23,28 @@ const followingSlice = createSlice({
         allFollowings:[],
         error :null
     },
-    reducers:{},
+    reducers:{
+        removeFollowingFromLoggedInUser(state,action){
+            // console.log("clcikeddddd",action,current(state))
+            const userUpdatedFollowingList = state.userfollowings.filter((user)=>user._id !== action.payload._id)
+            state.userfollowings = userUpdatedFollowingList
+        },
+        removeLoggedInUserFromFollowings(state,action){
+            const updatedAllFollowingsList = state.allFollowings.map((list)=>(
+                list._id === action.payload.user ? {
+                    ...list,
+                    userDetails:list.userDetails.filter((user)=>user._id !== action.payload.admin)
+                } :list
+            ))
+            state.allFollowings = updatedAllFollowingsList
+        },
+        addFollowingToLoggedInUser(state,action){
+            const getUser = state.allFollowings.filter((user)=>user._id === action.payload.accountId)
+            const getParticularUser = getUser[0].userDetails.filter((user)=>user._id === action.payload.userToBeFollowed)
+            state.userfollowings.push(getParticularUser[0])
+     
+        }
+    },
     extraReducers:{
         [getUserFollowingsFromServer.pending]: (state, action) => {
             state.status = "sending request to get user followings"
@@ -47,5 +68,7 @@ const followingSlice = createSlice({
         },
     }
 })
+
+export const {removeFollowingFromLoggedInUser,removeLoggedInUserFromFollowings,addFollowingToLoggedInUser} = followingSlice.actions;
 
 export default followingSlice.reducer;
